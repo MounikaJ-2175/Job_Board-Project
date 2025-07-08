dc7c2be164e72b64ff9aa2214c1ddacf352174f2
Mounika2175 <mounikasaisree15@gmail.com>
2025-07-08 12:33:53 +0530

    Connected frontend to deployed backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://job-board-project-2-2g72.onrender.com/api',
 });
 
 API.interceptors.request.use((req) => {
